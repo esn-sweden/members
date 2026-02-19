4153857a9972830872d89bbc0e302eb66f2ba88f
Elias Jollivet <63159238+Lurbyte@users.noreply.github.com>
2026-02-19 03:39:02 +0100

    Create members statistics function

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './styles.scss'
+import esnSwedenLogo from './esn-sweden.png'
+import 'bootstrap';
+import { countMembers } from './members.ts'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div class="container py-5">
+  <img src="${esnSwedenLogo}" class="d-block mx-auto img-fluid pb-4" style="max-width: 160px;" alt="ESN Sweden logo">
+
+  <h1>Member Statistics</h1>
+
+
+  <div id="membersContent"></div>
+
+  <p class="text-muted small mt-4 text-center">
+    All calculations are made in-browser. No data is sent to ESN Sweden.
+  </p>
+</div>
+`
+
+countMembers(document.querySelector<HTMLDivElement>('#membersContent')!)
---
+++ src/members.ts
@@ -0,0 +1,111 @@
+import Personnummer from 'personnummer';
+
+export function countMembers(app: HTMLElement) {
+    app.innerHTML = `
+<div class="mb-3">
+  <label for="pnInput" class="form-label">
+    Paste a list of the personal identity numbers (personnummer) of your members below:
+  </label>
+  <textarea id="pnInput" class="form-control mb-3" rows="6" placeholder="One personal number per line">
+</textarea>
+
+  <div class="card bg-light border-0 mb-3">
+    <div class="card-body p-3">
+
+      <div class="d-flex justify-content-between py-2 border-bottom">
+        <span>Total Members</span>
+        <strong id="totalCount">0</strong>
+      </div>
+
+      <div class="d-flex justify-content-between py-2 border-bottom">
+        <span>Men</span>
+        <strong id="menCount">0</strong>
+      </div>
+
+      <div class="d-flex justify-content-between py-2 border-bottom">
+        <span>Women</span>
+        <strong id="womenCount">0</strong>
+      </div>
+
+      <div class="d-flex justify-content-between py-2 border-bottom">
+        <span>Under 26</span>
+        <strong id="under26Count">0</strong>
+      </div>
+
+      <div class="d-flex justify-content-between py-2">
+        <span>Invalid Personal Identity Numbers</span>
+        <strong id="invalidCount">0</strong>
+      </div>
+
+    </div>
+  </div>
+
+  <div id="invalidContainer" class="card alert alert-danger border-0 mt-3 d-none">
+    <div class="card-body">
+      <h6 class="text-danger fw-semibold mb-3">
+        Invalid Personal Identity Numbers
+      </h6>
+      <ul id="invalidList"></ul>
+    </div>
+  </div>
+</div>
+  `;
+
+    const input = app.querySelector<HTMLTextAreaElement>("#pnInput")!;
+    const totalEl = app.querySelector<HTMLSpanElement>("#totalCount")!;
+    const menEl = app.querySelector<HTMLSpanElement>("#menCount")!;
+    const womenEl = app.querySelector<HTMLSpanElement>("#womenCount")!;
+    const under26El = app.querySelector<HTMLSpanElement>("#under26Count")!;
+    const invalidEl = app.querySelector<HTMLSpanElement>("#invalidCount")!;
+    const invalidContainer = app.querySelector<HTMLDivElement>("#invalidContainer")!;
+    const invalidList = app.querySelector<HTMLUListElement>("#invalidList")!;
+
+    input.addEventListener("input", () => {
+        const lines = input.value
+            .split(/\r?\n/)
+            .map(l => l.trim())
+            .filter(l => l.length > 0);
+
+        let total = 0;
+        let men = 0;
+        let women = 0;
+        let under26 = 0;
+        let invalid = 0;
+        let invalidPn: string[] = [];
+
+        for (const line of lines) {
+            // Personnummer does not work for temporary number with letters e.g. T or R by default
+            const normPn = line.replace(/[A-Za-z]/, "1");
+
+            if (!Personnummer.valid(normPn)) {
+                invalid++;
+                invalidPn.push(line);
+                continue;
+            }
+
+            total++;
+
+            const pn = Personnummer.parse(normPn);
+            if (pn.isMale()) men++;
+            if (pn.isFemale()) women++;
+            if (pn.getAge() < 26) under26++;
+
+        }
+
+        totalEl.textContent = String(total);
+        menEl.textContent = String(men);
+        womenEl.textContent = String(women);
+        under26El.textContent = String(under26);
+        invalidEl.textContent = String(invalid);
+
+
+        invalidList.innerHTML = "";
+        for (const num of invalidPn) {
+            const li = document.createElement("li");
+            li.textContent = num;
+            invalidList.appendChild(li);
+        }
+
+        invalidContainer.classList.toggle("d-none", invalidPn.length === 0);
+    });
+}
